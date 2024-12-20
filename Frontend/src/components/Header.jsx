74e2da223bd7e3b341058f8b90875e1eee7062d0
ayaz2004 <ayaznazir03@gmail.com>
2024-12-20 20:43:11 +0530

    Updated header component with user data

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,9 +1,19 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  Navbar,
+  TextInput,
+  Avatar,
+  DropdownDivider,
+} from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
+import { useSelector } from "react-redux";
 
 export default function Header() {
   const path = useLocation().pathname;
+  const { user: currentUser } = useSelector((state) => state.auth);
   return (
     <Navbar className="border-b-2">
       <Link
@@ -25,11 +35,34 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Link to="/SignIn">
-          <Button gradientDuoTone="redToYellow" outline>
-            Sign In
-          </Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt=" user" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.displayName}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"./dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign Out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/SignIn">
+            <Button gradientDuoTone="redToYellow" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
